c41051df2850a2c792df2b88a7ed012aa3c93e24
zy d <xydzy@mail.ustc.edu.cn>
2023-12-01 21:51:26 +0800

    init commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,18 @@
-import { defaultTheme, defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
 
-export default defineUserConfig({
-  base: 'start',
+export default {
+  base: '/',
   lang: 'zh-CN',
   title: 'xydzy的blog',
   description: 'xydzy的博客',
+  head: [
+    [
+      'link',{ rel: 'icon', href: 'blog_logo.jpg' }
+    ]
+  ],
+  port: 3333,
   theme: defaultTheme({
+    logo: 'blog_logo.jpg',
     navbar:[
         {text: '代码', link: '/code/'},
         {text: '数学', link: '/math/'},
@@ -14,15 +21,14 @@ export default defineUserConfig({
     sidebar: {
         '/code/': [
             'Intro',
-            'git',
+            'git'
         ],
         '/math/': [
-            'Intro',
+            'Intro'
         ],
         '/paper/': [
-            'Intro',
+            'Intro'
         ]
     }
-
   })
-})
+}
